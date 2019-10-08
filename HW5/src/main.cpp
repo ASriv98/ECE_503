a9ec6d14a8b4c2b01d929fafecc123bf88cfe54c
ASriv98 <atulsriv3@gmail.com>
2019-10-08 16:40:00 -0400

    HW 5 Part 1 working

---
+++ HW5/src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdexcept>
+#include <iomanip>
+#include <typeinfo>
+
+std::string square(int n)
+{
+	if(!(n >= 0))
+	{
+		throw std::invalid_argument("this function is expecting a positive integer");
+	}
+
+	std::string returnStatement;
+	std::string squared = std::to_string(n*n);
+
+	//std::cout << squared << std::endl;
+	if(n == 1)
+	{
+		returnStatement = squared;
+	}
+
+	else
+	{
+		if(n % 2 == 0)
+		{
+			returnStatement = square(n-1) + ", " + squared;
+		}
+		else
+		{
+			returnStatement = squared + ", " + square(n-1);
+		}
+	}
+
+	return returnStatement;
+}
+
+int main()
+{
+	int input = 0;
+	std::cout << "Please enter a positive number that you would like to run in recursive function: ";
+	std::cin  >> input;
+
+
+	std::string result = square(input);
+	std::cout << result << std::endl;
+
+	return 0;
+}
